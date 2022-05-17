ac22d290668b46edaaa7eb6794377e11dc301256
Otávio Vinícius Castro do Nascimento <viinident@gmail.com>
2022-05-17 16:44:08 -0300

    exercícios 4.2

---
+++ Fundamentos/Introdução à JavaScript e Lógica de Programação /exercicios-aula.js
@@ -0,0 +1,25 @@
+// Exercício Mega Sena
+
+// let numerosMegaSena = [1, 8, 6, 34, 17, 60];
+
+// let meusNumeros = [6, 15, 22, 38, 42, 57];
+
+// for (let i = 0; i < numerosMegaSena.length; i += 1) {
+//     console.log(numerosMegaSena[i])
+//     for (let contador = 0; contador < meusNumeros.length; contador += 1) {
+//         console.log(meusNumeros[contador])
+//     }
+// }
+
+
+// Exercício do maior número
+
+// let maior = [0];
+
+// let numeros = [80, 60, 92, 35];
+
+// for (let i = 0; i < numeros.length; i += 1) {
+//     if (maior < numeros[i]) {
+//         maior = numeros[i]
+//     }
+// }=-//     console.log(maior);
