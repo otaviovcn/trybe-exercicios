189cea28e41827e962664f04f6290257a5bb926f
Otavio Vinicius Castro do Nascimento <viinident@gmail.com>
2022-05-25 23:09:55 -0300

    teste 1

---
+++ Fundamentos/JavaScript: DOM, Eventos e Web Storage/script.js
@@ -0,0 +1,11 @@
+// let lista = document.getElementsByClassName("list")[0];
+//       lista.style.color = 'red';
+
+let list = document.getElementsByClassName('list');
+    list[0].innerText = 'Xablau'
+    list[1].style.color = 'green'
+    list[1].style.backgroundColor = 'yellow'
+    //  console.log(list1.innerText); 
+
+    let frase = document.getElementById('phrase');
+    frase.style.color = 'white'
