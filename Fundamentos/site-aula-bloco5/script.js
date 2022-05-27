02a5173f54a6fc2c3b67b00fa5c814a787a45a0c
Otavio Vinicius Castro do Nascimento <viinident@gmail.com>
2022-05-26 22:19:11 -0300

    commit 1

---
+++ Fundamentos/site-aula-bloco5/script.js
@@ -0,0 +1,13 @@
+// function teste () {
+//     console.log('Clicou');
+// }
+
+window.onload = function () {
+    let botao = document.querySelector('#start-race-btn');
+    console.log(botao);
+
+    botao.addEventListener('click', function(event) {
+        let car1 = document.querySelector('.car1');
+        let car2 = document.querySelector('.car2');
+    });
+}
