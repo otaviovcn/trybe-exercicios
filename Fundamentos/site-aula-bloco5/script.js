076af1fc795a138b4d01f783e09e7259979570ff
Otavio Vinicius Castro do Nascimento <viinident@gmail.com>
2022-05-27 16:06:50 -0300

    teste 2

--- Fundamentos/site-aula-bloco5/script.js
+++ Fundamentos/site-aula-bloco5/script.js
@@ -2,12 +2,28 @@
 //     console.log('Clicou');
 // }
 
-window.onload = function () {
-    let botao = document.querySelector('#start-race-btn');
-    console.log(botao);
+let car1 = document.querySelector('.car1');
+let car2 = document.querySelector('.car2');
 
-    botao.addEventListener('click', function(event) {
-        let car1 = document.querySelector('.car1');
-        let car2 = document.querySelector('.car2');
-    });
+car1.style.marginLeft = 0;
+car2.style.marginLeft = 0;
+
+window.onload = function () {
+  let botao = document.querySelector('#start-race-btn');
+  console.log(botao);
+
+  botao.addEventListener('click', function () {
+
+    car1.style.marginLeft = parseInt(car1.style.marginLeft) + Math.random() * 100 + 'px';
+    car2.style.marginLeft = parseInt(car2.style.marginLeft) + Math.random() * 100 + 'px';
+
+    if (parseInt(car1.style.marginLeft) > window.innerWidth){
+      alert('Carro 1 ganhou');
+    }
+
+    else if (parseInt(car2.style.marginLeft) > window.innerWidth){
+      alert('Carro 2 ganhou');
+    }    
+
+  });
 }
